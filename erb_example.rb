ee4f48f4d772b8e33beb3f838bca0f1105ec682a
Juan <jm2721@trevor.org>
2013-05-07 14:30:04 -0400

    Modified erb_example, added some loops, hashes

--- erb_example.rb
+++ erb_example.rb
@@ -1,5 +1,21 @@
 require 'erb'
 
 x = 42
-template = ERB.new "The value of x is: <%= x %>"
+myRA = ["movie1", "movie2", "movie3"]
+
+myRA.each_with_index do |movie, index|
+  template = ERB.new "The value of <%= #{index} %> is <%= myRA[#{index}] %>" 
+  puts template.result(binding)
+end
+
+numRA = [3, 4, 5, 2, 4, 5]
+
+numRA.each_with_index do |num, index|
+  template = ERB.new "Value of num multiplied by its index is <%= #{num}*#{index} %>"
+  puts template.result(binding)
+end
+# Name, age
+myhash = {:name => "Juan", :age => 20}
+template = ERB.new "<%= myhash[:name] %> is <%= myhash[:age] %> years old"
 puts template.result(binding)
+
